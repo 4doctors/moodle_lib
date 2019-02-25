170c346422dea507060cfc6a5361bb387e7fe12c
Iñigo Solano <i@errstate.com>
2019-02-25 15:51:10 +0100

    add the hability to create and delete cohorts

--- lib/moodle_lib/client/cohorts.ex
+++ lib/moodle_lib/client/cohorts.ex
@@ -9,6 +9,26 @@ defmodule MoodleLib.Client.Cohorts do
     |> process_got_cohort()
   end
 
+  def create_cohort(params) do
+    params
+    |> Map.put("categorytype][type", "system")
+    |> Map.put("categorytype][value", "ignored")
+    |> Enum.map(fn {k, v} -> {"cohorts[0][#{k}]", v} end)
+    |> Map.new()
+    |> Map.put(:wsfunction, :core_cohort_create_cohorts)
+    |> Common.build_uri()
+    |> HTTPoison.get!()
+    |> process_created_cohort()
+  end
+
+  def delete_cohort(id) do
+    %{"cohortids[0]" => id}
+    |> Map.put(:wsfunction, :core_cohort_delete_cohorts)
+    |> Common.build_uri()
+    |> HTTPoison.get!()
+    |> process_cohort_deleted()
+  end
+
   defp process_got_cohort(%HTTPoison.Response{body: body}) do
     parsed_body = body |> Jason.decode!(keys: :atoms)
 
@@ -18,6 +38,30 @@ defmodule MoodleLib.Client.Cohorts do
 
       [] ->
         {:error, message: "Cohort not found"}
+    end
+  end
+
+  defp process_created_cohort(%HTTPoison.Response{body: body}) do
+    parsed_body = body |> Jason.decode!(keys: :atoms)
+
+    case parsed_body do
+      [%{} = cohort] ->
+        {:ok, cohort}
+
+      %{message: message} ->
+        {:error, message: message}
+    end
+  end
+
+  defp process_cohort_deleted(%HTTPoison.Response{body: body}) do
+    parsed_body = body |> Jason.decode!(keys: :atoms)
+
+    case parsed_body do
+      nil ->
+        {:ok, message: "Cohort successfully deleted"}
+
+      _ ->
+        {:error, message: "Error deleting the cohort"}
     end
   end
 end
--- test/moodle_lib/client/cohorts_test.exs
+++ test/moodle_lib/client/cohorts_test.exs
@@ -3,10 +3,34 @@ defmodule MoodleLib.Client.CohortsTest do
 
   alias MoodleLib.Client.Cohorts
 
-  test "it can retrieve a cohort given its id" do
-    {:ok, cohort} = Cohorts.get_cohort(1)
+  @default_params %{
+    name: "Test",
+    idnumber: "test"
+  }
 
-    assert cohort.id == 1
+  test "it can retrieve a cohort given its id" do
+    {:ok, org} = Cohorts.create_cohort(@default_params)
+
+    {:ok, cohort} = Cohorts.get_cohort(org.id)
+
+    on_exit(fn -> Cohorts.delete_cohort(cohort.id) end)
+
+    assert cohort.id == org.id
     assert cohort.name == "Test"
+  end
+
+  test "it can create a cohort" do
+    {:ok, cohort} = Cohorts.create_cohort(@default_params)
+
+    on_exit(fn -> Cohorts.delete_cohort(cohort.id) end)
+
+    assert cohort.name == "Test"
+    assert cohort.idnumber == "test"
+  end
+
+  test "it can delete a cohort given its id" do
+    {:ok, cohort} = Cohorts.create_cohort(@default_params)
+
+    assert {:ok, _message} = Cohorts.delete_cohort(cohort.id)
   end
 end
