826abee283daac3b74787cbcc72305abbb8fc1b8
Iñigo Solano <i@errstate.com>
2019-02-27 09:36:54 +0100

    refactor tests to use exvcr and create as few cassettes as possible

--- test/moodle_lib/client/cohorts_test.exs
+++ test/moodle_lib/client/cohorts_test.exs
@@ -1,5 +1,6 @@
 defmodule MoodleLib.Client.CohortsTest do
   use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias MoodleLib.Client.{Cohorts, Users}
 
@@ -8,55 +9,47 @@ defmodule MoodleLib.Client.CohortsTest do
     idnumber: "test"
   }
 
-  test "it can retrieve a cohort given its id" do
-    {:ok, org} = Cohorts.create_cohort(@default_params)
+  test "it can create/get/delete cohorts" do
+    use_cassette "managing_cohorts", match_requests_on: [:query] do
+      {:ok, org} = Cohorts.create_cohort(@default_params)
+      assert org.name == "Test"
+      assert org.idnumber == "test"
 
-    {:ok, cohort} = Cohorts.get_cohort(org.id)
+      {:ok, cohort} = Cohorts.get_cohort(org.id)
+      assert cohort.id == org.id
+      assert cohort.name == "Test"
 
-    on_exit(fn -> Cohorts.delete_cohort(cohort.id) end)
-
-    assert cohort.id == org.id
-    assert cohort.name == "Test"
+      assert {:ok, _message} = Cohorts.delete_cohort(cohort.id)
+    end
   end
 
-  test "it can create a cohort" do
-    {:ok, cohort} = Cohorts.create_cohort(@default_params)
+  test "it can manage cohort users" do
+    use_cassette "managing_cohort_users", match_requests_on: [:query] do
+      {:ok, user} =
+        Users.create_user(%{
+          username: "john",
+          email: "j.doe@example.com",
+          firstname: "John",
+          lastname: "Doe"
+        })
 
-    on_exit(fn -> Cohorts.delete_cohort(cohort.id) end)
+      {:ok, cohort} = Cohorts.create_cohort(@default_params)
 
-    assert cohort.name == "Test"
-    assert cohort.idnumber == "test"
-  end
+      {:ok, users} = Cohorts.add_user_to_cohort(cohort, user)
+      assert Enum.member?(users, user.id)
 
-  test "it can delete a cohort given its id" do
-    {:ok, cohort} = Cohorts.create_cohort(@default_params)
+      # we need to use on_exit because it runs on a separate process
+      # so we can record the same call with the right results
+      # remove_user_from_cohort calls get_cohort_members under the hood
+      on_exit(fn ->
+        use_cassette "managing_cohort_users-cleanup", match_requests_on: [:query] do
+          {:ok, users} = Cohorts.remove_user_from_cohort(cohort, user)
+          refute Enum.member?(users, user.id)
 
-    assert {:ok, _message} = Cohorts.delete_cohort(cohort.id)
-  end
-
-  test "it can add a user to the cohort" do
-    {:ok, user} =
-      Users.create_user(%{
-        username: "john",
-        email: "j.doe@example.com",
-        firstname: "John",
-        lastname: "Doe"
-      })
-
-    {:ok, cohort} = Cohorts.create_cohort(@default_params)
-
-    {:ok, users} = Cohorts.get_cohort_members(cohort.id)
-    refute Enum.member?(users, user.id)
-
-    {:ok, users} = Cohorts.add_user_to_cohort(cohort, user)
-    assert Enum.member?(users, user.id)
-
-      {:ok, users} = Cohorts.remove_user_from_cohort(cohort, user)
-    refute Enum.member?(users, user.id)
-
-    on_exit(fn ->
-      Users.delete_user(user.id)
-      Cohorts.delete_cohort(cohort.id)
-    end)
+          {:ok, _} = Users.delete_user(user.id)
+          {:ok, _} = Cohorts.delete_cohort(cohort.id)
+        end
+      end)
+    end
   end
 end
