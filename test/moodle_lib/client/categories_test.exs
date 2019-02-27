8705cf8c03da6b2373c9b9f08b39a7bad69c72bf
Iñigo Solano <i@errstate.com>
2019-02-27 18:30:26 +0100

    record categories query with exvcr

--- test/moodle_lib/client/categories_test.exs
+++ test/moodle_lib/client/categories_test.exs
@@ -1,11 +1,14 @@
 defmodule MoodleLib.Client.CategoriesTest do
   use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias MoodleLib.Client.Categories
 
   test "it can retrieve the list of all categories" do
-    {:ok, categories} = Categories.get_categories()
+    use_cassette "get_categories", match_requests_on: [:query] do
+      {:ok, categories} = Categories.get_categories()
 
-    assert Enum.count(categories) > 0
+      assert Enum.count(categories) > 0
+    end
   end
 end
