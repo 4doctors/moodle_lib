e98d549dc8edc5aa4a6aec8588700f79abf22590
Iñigo Solano <i@errstate.com>
2019-03-25 09:54:30 +0100

    delete unused autogenerated code

--- lib/moodle_lib.ex
+++ lib/moodle_lib.ex
@@ -1,18 +1,2 @@
 defmodule MoodleLib do
-  @moduledoc """
-  Documentation for MoodleLib.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MoodleLib.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
