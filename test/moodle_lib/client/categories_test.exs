a8509cd84b3d479600ceebc99744227b466e5037
Iñigo Solano <i@errstate.com>
2019-06-03 12:32:04 +0200

    improve categories test

--- test/moodle_lib/client/categories_test.exs
+++ test/moodle_lib/client/categories_test.exs
@@ -5,9 +5,13 @@ defmodule MoodleLib.Client.CategoriesTest do
   alias MoodleLib.Client.Categories
 
   test "it can retrieve the list of all categories" do
+    # right now categories are preseeded on the DB: category 1, category 1.1 and category 2
     use_cassette "get_categories", match_requests_on: [:query] do
       {:ok, categories} = Categories.get_categories()
 
+      assert Enum.any?(categories, &(&1.name == "category 1"))
+      assert Enum.any?(categories, &(&1.name == "category 1.1"))
+      assert Enum.any?(categories, &(&1.name == "category 2"))
       assert Enum.count(categories) > 0
     end
   end
