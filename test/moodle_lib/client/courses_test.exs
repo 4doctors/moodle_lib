ad7163a37619aa2ee3debee6f746d329fe3c6263
Iñigo Solano <i@errstate.com>
2019-06-03 12:33:26 +0200

    record the exvcr cassette for courses

--- test/moodle_lib/client/courses_test.exs
+++ test/moodle_lib/client/courses_test.exs
@@ -6,12 +6,12 @@ defmodule MoodleLib.Client.CoursesTest do
 
   test "it can retrieve the list of all categories" do
     # right now courses are preseeded on the DB: Test Course 1 ("testcourse1") belonging to category 1.1 and Test Course 2 (testcourse2) category 2
-    # use_cassette "get_categories", match_requests_on: [:query] do
-    {:ok, courses} = Courses.get_courses()
+    use_cassette "get_courses", match_requests_on: [:query] do
+      {:ok, courses} = Courses.get_courses()
 
-    assert Enum.any?(courses, &(&1.shortname == "testcourse1"))
-    assert Enum.any?(courses, &(&1.shortname == "testcourse2"))
-    assert Enum.count(courses) > 0
-    # end
+      assert Enum.any?(courses, &(&1.shortname == "testcourse1"))
+      assert Enum.any?(courses, &(&1.shortname == "testcourse2"))
+      assert Enum.count(courses) > 0
+    end
   end
 end
